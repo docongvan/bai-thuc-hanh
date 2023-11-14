34b39dd496054c7dbbfc879afc7c37ab04c7eb5c
Van <docongvan123455@gamil.com>
2023-11-14 15:54:01 +0700

    PHP slide 29

--- PHP/index.php
+++ PHP/index.php
@@ -1,11 +1,11 @@
 <?php
-  $Subject = "Lap trinh ung dung web";
-  $universityName = "HUMG";
-  $firstNumber = 2;
-  $secondNumber = 2;
+ $Subject = "Lap trinh ung dung web";
+ $universityName = "HUMG";
+ $firstNumber = 2;
+ $secondNumber = 2;
 
-  echo "Hoc ". $Subject . "o phong 502 tai truong " . $universityName . "<br>";
-  echo "Hoc  $Subject o phong 502 tai truong $universityName <br>";
-  echo "<h1> $universityName </h1>";
-  echo $firstNumber + $secondNumber;
+ echo "Hoc ". $Subject . "o phong 502 tai truong " . $universityName . "<br>";
+ echo "Hoc  $Subject oo phong 502 tai truong $universityName <br>";
+ echo "<h1> $universityName </h1>";
+ echo $firstNumber + $secondNumber;
 ?>
