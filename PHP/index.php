916f6a90a936ce7cb7a87c199872df4b42c9b712
Van <docongvan123455@gamil.com>
2023-11-17 10:21:11 +0700

    PHP slide 34

--- PHP/index.php
+++ PHP/index.php
@@ -1,10 +1,20 @@
-<?php
-  $a = 5;
-  $b = 7;
-  if ($a == $b) {
-      echo "Day la hinh vuong";
-  }
-  else {
-      echo "Day la hinh chu nhat";
-  }
-  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="5-formGet.php" method="get">
+        <p>Name : </p>
+        <input type="text" name="name"><br>
+
+        <p>E-mail : </p>
+        <input type="text" name="email"><br>
+
+        <p></p>
+        <input type="submit">
+    </form>
+</body>
+</html>
