75a0b15dc45e16d13842a1998f8fc2d8e6f9fca1
Van <docongvan123455@gamil.com>
2023-11-17 10:16:03 +0700

    PHP slide 30-4

--- PHP/index.php
+++ PHP/index.php
@@ -1,10 +1,10 @@
 <?php
-$str = "chung toi la sinh vien truong dai hoc mo dai chat";
-$count = 0;
-for ($i = 0; $i < strlen($str); $i++) {
-    if ($str[$i] == 'h') {
-        $count++;
-    }
-}
-    echo "Chuoi tren co " . $count. " chu 'h'";
-?>
+  $a = 5;
+  $b = 7;
+  if ($a == $b) {
+      echo "Day la hinh vuong";
+  }
+  else {
+      echo "Day la hinh chu nhat";
+  }
+  ?>
