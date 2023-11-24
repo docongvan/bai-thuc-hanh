cc1fe547ed1042d9e271c82792b328e03a5d3006
Van <docongvan123455@gamil.com>
2023-11-24 10:14:29 +0700

    PHP slide 52

--- PHP/index.php
+++ PHP/index.php
@@ -3,12 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Đăng ký thành viên</title>
 </head>
 <body>
-    Resulr:
-    <?php $result = $_GET["firdtNumber"] + $_GET["secondNumber"];
-    echo $result;
-    ?>
+    <h2>Đăng ký thành viên</h2>
+    <form action="result.php" method="post">
+        <label for="username">Tên đăng nhập:</label>
+        <input type="text" name="username" required><br>
+        <label for="password">Mật khẩu:</label>
+        <input type="password" name="password" required><br>
+        <label for="confirm_password">Xác nhận mật khẩu:</label>
+        <input type="password" name="confirm_password" required><br>
+        <input type="submit" value="Đăng ký">
+    </form>
 </body>
 </html>
