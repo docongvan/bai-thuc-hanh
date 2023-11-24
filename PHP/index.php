173c51b29937af6643bcebd557a4ed900b93200b
Van <docongvan123455@gamil.com>
2023-11-24 10:22:50 +0700

    PHP slide 2

--- PHP/index.php
+++ PHP/index.php
@@ -1,7 +1,25 @@
-<?php
-$items = array('items1.png' , 'items2.png' , 'items3.png' , 'items4.png');
-foreach ($items as $items)
-{
-    echo "<img src ='images/$item' width = 50pc height = 50px alt='$item'>";
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $T = True ;
+    $F = False ;
+    echo "T = $T  <br>";
+    echo "F = $F  <br>";
+
+    $check1 = 10 > 5 ; 
+    $check2 = 20 < 10 ; 
+    $check3 = $check1 && $check2 ; 
+    $check4 = $check1 || $check2 ; 
+     echo "check1 = $check1 <br>";
+     echo "check2 = $check2 <br>";
+     echo "check3 = $check3 <br>";
+     echo "check4 = $check4 ";
+    ?>
+</body>
+</html>
