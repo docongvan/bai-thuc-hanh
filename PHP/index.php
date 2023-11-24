5e76034db35d6d491b495ff8bc995d2672e82f45
Van <docongvan123455@gamil.com>
2023-11-24 10:25:40 +0700

    PHP slide 8

--- PHP/index.php
+++ PHP/index.php
@@ -2,24 +2,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <?php
-    $T = True ;
-    $F = False ;
-    echo "T = $T  <br>";
-    echo "F = $F  <br>";
-
-    $check1 = 10 > 5 ; 
-    $check2 = 20 < 10 ; 
-    $check3 = $check1 && $check2 ; 
-    $check4 = $check1 || $check2 ; 
-     echo "check1 = $check1 <br>";
-     echo "check2 = $check2 <br>";
-     echo "check3 = $check3 <br>";
-     echo "check4 = $check4 ";
-    ?>
+    <h3>quit the game ?</h3>
+    <form action="8-ifResult.php" method="get">
+        <select name="response" id="">
+            <option value="yes">yes</option>
+            <option value="no">no</option>
+        </select>
+        <input type="submit" value="submit">
+    </form>
 </body>
 </html>
