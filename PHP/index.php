1ecdcd9621c52d2d76d0bc26baf355a7163ceb68
Van <docongvan123455@gamil.com>
2023-11-24 10:17:56 +0700

    PHP slide 3

--- PHP/index.php
+++ PHP/index.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Đăng ký thành viên</title>
-</head>
-<body>
-    <h2>Đăng ký thành viên</h2>
-    <form action="result.php" method="post">
-        <label for="username">Tên đăng nhập:</label>
-        <input type="text" name="username" required><br>
-        <label for="password">Mật khẩu:</label>
-        <input type="password" name="password" required><br>
-        <label for="confirm_password">Xác nhận mật khẩu:</label>
-        <input type="password" name="confirm_password" required><br>
-        <input type="submit" value="Đăng ký">
-    </form>
-</body>
-</html>
+<?php
+$items = array('items1.png' , 'items2.png' , 'items3.png' , 'items4.png');
+foreach ($items as $items)
+{
+    echo "<img src ='images/$item' width = 50pc height = 50px alt='$item'>";
+}
+?>
