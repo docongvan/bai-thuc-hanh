8ac77931500c5bef0744dfa43fe916ad5f0b941b
Van <docongvan123455@gamil.com>
2023-11-28 16:13:38 +0700

    PHP slide 28

--- PHP/index.php
+++ PHP/index.php
@@ -3,54 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chọn giới tính</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            text-align: center;
-            margin: 50px;
-        }
-        #result {
-            font-weight: bold;
-            color: green;
-        }
-        #error {
-            color: red;
-        }
-    </style>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Chọn giới tính và nhập thông tin</h1>
-    <form method="post" action="">
-        <label for="gender">Giới tính:</label>
-        <select name="gender" id="gender">
-            <option value="male">Nam</option>
-            <option value="female">Nữ</option>
-        </select>
-        <br><br>
-        <label for="height">Chiều cao (cm):</label>
-        <input type="number" name="height" id="height" placeholder="Nhập chiều cao">
-        <br><br>
-        <label for="weight">Cân nặng (kg):</label>
-        <input type="number" name="weight" id="weight" placeholder="Nhập cân nặng">
-        <br><br>
-        <button type="submit">Kiểm tra</button>
-    </form>
-    <br><br>
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $gender = $_POST['gender'];
-        $height = floatval($_POST['height']);
-        $weight = floatval($_POST['weight']);
-
-        if ($gender == 'male' && $height > 160 && $weight > 50) {
-            echo '<div id="result">Yêu cầu đạt được: Nam, chiều cao > 160cm, cân nặng > 50kg</div>';
-        } elseif ($gender == 'female' && $height > 150 && $weight > 40 && $weight < 70) {
-            echo '<div id="result">Yêu cầu đạt được: Nữ, chiều cao > 150cm, 40kg < cân nặng < 70kg</div>';
-        } else {
-            echo '<div id="error">Không đạt được yêu cầu</div>';
-        }
-    }
+    <?php 
+    $exchangeRate = array(22300, 27300, 17000, 120 );
+    $exchangeRate[] = 600 ;
+    $exchangeRate[] = 16000 ;
+    $exchangeRate[0] = 22200 ;
+    echo "<h3> Exchange rate vnd </h3>";
+    echo "usd : $exchangeRate[0]<br>";
+    echo "eur : $exchangeRate[1]<br>";
+    echo "sgd : $exchangeRate[2]<br>";
+    echo "jpy : $exchangeRate[3]<br>";
+    echo "chn : $exchangeRate[4]<br>";
+    echo "aud : $exchangeRate[5]<br>";
     ?>
 </body>
 </html>
