4a1718a59f119d22351d4131dffa725e957dfd2d
Van <docongvan123455@gamil.com>
2023-11-28 10:56:33 +0700

    PHP slide 17

--- PHP/index.php
+++ PHP/index.php
@@ -6,16 +6,18 @@
     <title>Document</title>
 </head>
 <body>
-    <?php echo "the game ";
-    if ($_GET["response"] == "yes")
-    {
-        echo "has been quited ";
-    } 
-    else
-    {
-        echo " will be continued in 3 second ";
-    }
-    echo "<br> after if statment ";
-    ?>
+   <h3>doi tien </h3>
+   <form action="result.php " method="get ">
+    <label for="amount"> so tien :</label>
+    <input type="number" name="amount"> <br>
+    <label for="currency"> chon loai tien :</label>
+    <select name="currency" required>
+        <option value="usd">usd - 23,000 vnd</option>
+        <option value="aud">aud - 17,000 vnd </option>
+        <option value="jpy">jpy - 200 vnd</option>
+        <option value="eur">eur - 27,000 vnd </option>
+    </select><br>
+    <input type="submit" value="chuyen doi ">
+   </form>
 </body>
 </html>
