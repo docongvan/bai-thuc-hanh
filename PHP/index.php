84828f3edfb04ec03be4adcda7c4ec606c5e10d3
Van <docongvan123455@gamil.com>
2023-11-28 16:10:53 +0700

    PHP slide 23

--- PHP/index.php
+++ PHP/index.php
@@ -3,33 +3,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>it is chrismas ?</title>
+    <title>Chọn giới tính</title>
     <style>
-     html {
-        background-color: black;
-        color: azure;
-        font-size: 300px ;
-        margin: 0 auto;
-     }
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin: 50px;
+        }
+        #result {
+            font-weight: bold;
+            color: green;
+        }
+        #error {
+            color: red;
+        }
     </style>
 </head>
 <body>
-     <?php 
-     // lay ngay thang hien tai
-     $date = date ("d") ;
-     $month = date ("m") ;
-     //ngay thang giang sinh 
-     $datec = "25";
-     $mothc = "12";
-     // kiem tra neu ngay hom nay = ngay giang sinh thif hirrnj dung ko sai 
-     if ($date == $datec && $month == $monthc) 
-     {
-        echo "dung";
-     }
-     else
-     {
-        echo "sai";
-     }
-     ?>
+    <h1>Chọn giới tính và nhập thông tin</h1>
+    <form method="post" action="">
+        <label for="gender">Giới tính:</label>
+        <select name="gender" id="gender">
+            <option value="male">Nam</option>
+            <option value="female">Nữ</option>
+        </select>
+        <br><br>
+        <label for="height">Chiều cao (cm):</label>
+        <input type="number" name="height" id="height" placeholder="Nhập chiều cao">
+        <br><br>
+        <label for="weight">Cân nặng (kg):</label>
+        <input type="number" name="weight" id="weight" placeholder="Nhập cân nặng">
+        <br><br>
+        <button type="submit">Kiểm tra</button>
+    </form>
+    <br><br>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $gender = $_POST['gender'];
+        $height = floatval($_POST['height']);
+        $weight = floatval($_POST['weight']);
+
+        if ($gender == 'male' && $height > 160 && $weight > 50) {
+            echo '<div id="result">Yêu cầu đạt được: Nam, chiều cao > 160cm, cân nặng > 50kg</div>';
+        } elseif ($gender == 'female' && $height > 150 && $weight > 40 && $weight < 70) {
+            echo '<div id="result">Yêu cầu đạt được: Nữ, chiều cao > 150cm, 40kg < cân nặng < 70kg</div>';
+        } else {
+            echo '<div id="error">Không đạt được yêu cầu</div>';
+        }
+    }
+    ?>
 </body>
 </html>
