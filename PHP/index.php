405adbc549031071403d0287748c44a04eda2ac8
Van <docongvan123455@gamil.com>
2023-12-08 08:37:04 +0700

    PHP slide 30

--- PHP/index.php
+++ PHP/index.php
@@ -6,18 +6,15 @@
     <title>Document</title>
 </head>
 <body>
-    <?php 
-    $exchangeRate = array(22300, 27300, 17000, 120 );
-    $exchangeRate[] = 600 ;
-    $exchangeRate[] = 16000 ;
-    $exchangeRate[0] = 22200 ;
-    echo "<h3> Exchange rate vnd </h3>";
-    echo "usd : $exchangeRate[0]<br>";
-    echo "eur : $exchangeRate[1]<br>";
-    echo "sgd : $exchangeRate[2]<br>";
-    echo "jpy : $exchangeRate[3]<br>";
-    echo "chn : $exchangeRate[4]<br>";
-    echo "aud : $exchangeRate[5]<br>";
+    <?php
+    $exchangeRate = array("USD"=>22300, "EUR"=>27300, "SGD"=>17000 , "JPY"=>120);
+    $exchangeRate[] = 600;
+    $exchangeRate[] = 16000;
+    $exchangeRate[0] = 22200;
+    echo "<h3>Exchange rate to VND</h3>";
+    echo "USD:" .$exchangeRate['USD'] . "<br>";
+    echo "EUR:" .$exchangeRate['EUR'] . "<br>"; 
+    echo "SGD:" .$exchangeRate['SGD'] . "<br>"; 
+    echo "JPY:" .$exchangeRate['JPY'] . "<br>"; 
     ?>
 </body>
-</html>
